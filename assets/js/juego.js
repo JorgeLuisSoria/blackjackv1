24e8cee53d9afaeb1f2a3dc5c594c50534737a51
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2023-12-17 16:22:53 -0500

    JS files added:buildDeck, getCard and getCardValue

---
+++ assets/js/juego.js
@@ -0,0 +1,65 @@
+/*
+*2C = Two of clubs
+*2H = Two of hearts
+*2D = Two of diamonds
+*2S = Two of spades
+*/
+
+let deck            = [];
+const tipos         = ['C', 'D', 'H', 'S'];
+const especiales    = ['A', 'J', 'Q', 'K'];
+
+// Esta función crea un nuevo deck
+
+const crearDeck = () => {
+
+    for( let i = 2; i <= 10; i++ ) {
+        
+        for( let tipo of tipos ) {
+            deck.push( i + tipo );
+        }
+        
+    }
+
+    for( let tipo of tipos ) {
+        for( let esp of especiales ) {
+            deck.push( esp + tipo );
+        }
+    }
+     
+    deck = _.shuffle( deck );    
+    console.log( deck );
+    return deck;
+}
+
+crearDeck();
+
+// Esta función permite tomar una carta
+
+const pedirCarta = () => {
+
+    if( deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log( carta );
+    return carta;
+}
+
+// pedirCarta();
+
+const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+
+    return ( isNaN( valor ) ) ?
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log({ valor });
